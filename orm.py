41f6c5fb7a28191469bda12d501f3101322e124d
Cameron Dykstra <dykstra.cameron@gmail.com>
2016-11-22 17:21:55 +1300

    initial commit with the ORM for an SQLite database

---
+++ orm.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, Integer, Enum, Sequence, Text, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import enum
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///pubsub.db"
+
+Base = declarative_base()
+
+class TypeEnum(enum.Enum):
+    game = 'game'
+    milestone = 'milestone'
+
+# Object defs
+
+class Event(Base):
+    __tablename__ = 'event'
+    id = Column(Integer, Sequence('event_id_seq'), primary_key=True)
+    type = Column(Enum(TypeEnum), nullable=False)
+    data = Column(Text, nullable=False)
+    time = Column(DateTime, nullable=False)
+    src_abbr = Column(String(10), nullable=False)
+    src_url = Column(String(1000), nullable=False)
+
+    def __repr__(self):
+        return "<Event(id={event.id}, type={event.type}, time={event.time}, src_abbr={event.src_abbr}, src_url={event.src_url}, data={event.data})>".format(event=self)
+
+# End Object defs
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
+session = sessionmaker(expire_on_commit=False)
+session.configure(bind=engine)
+Base.metadata.create_all(engine)
+
+def get_session():
+    return session()
---
+++ server.py
@@ -0,0 +1,7 @@
+import orm
+from orm import Event
+
+
+
+if __name__=='__main__':
+    sess = orm.get_session()
